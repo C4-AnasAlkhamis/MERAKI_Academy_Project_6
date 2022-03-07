02b26a6062e5436ad88c54d8c25ebeb050301efa
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-03-07 11:27:38 +0300

    create login function

---
+++ backend/routes/login.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const {login} = require("../controllers/login");
+const loginRouter = express.Router();
+//dont press enter
+//write your code here
+
+loginRouter.post("/", login);
+
+//write your code here
+module.exports = loginRouter;
--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,19 @@ app.use(cors());
 app.use(express.json());
 
 const userRouter = require("./routes/register");
+const loginRouter = require("./routes/login");
+
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static("client/build"));
-// }
+// }loginRouter
 
 const PORT = process.env.PORT || 5000;
 
 //middleware
 
 app.use("/register", userRouter);
+app.use("/login", loginRouter);
+
 // app.get("*", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 // });

dbf5a651a167e79bc9b3fb39915ddd43ac59f534
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-03-09 14:54:15 +0300

    login form using bootstarp

--- client/src/component/login/Login.jsx
+++ client/src/component/login/Login.jsx
@@ -4,14 +4,14 @@ import { useNavigate } from "react-router-dom";
 import "./login.css";
 import { logIn } from "../../reducer/login/index";
 import { useDispatch } from "react-redux";
+import { FloatingLabel, Form, Button } from "react-bootstrap";
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const verifyUser = async (e) => {
-    e.preventDefault();
+  const verifyUser = async () => {
     await axios
       .post("http://localhost:5000/login", {
         email,
@@ -21,13 +21,10 @@ const Login = () => {
         if (result) {
           localStorage.setItem("token", result.data.token);
           localStorage.setItem("user_name", result.data.name);
-
           setEmail("");
           setPassword("");
-
-          navigate(`/home`);
           dispatch(logIn(result.data.token));
-          console.log(result);
+          navigate(`/home`);
         }
       })
       .catch((err) => {});
@@ -35,7 +32,38 @@ const Login = () => {
 
   return (
     <>
-      <div className="login_box">
+      <FloatingLabel
+        controlId="floatingInput"
+        label="Email address"
+        className="mb-3"
+      >
+        <Form.Control
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
+          required
+          autoComplete="off"
+          type="email"
+          placeholder="name@example.com"
+        />
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingPassword" label="Password">
+        <Form.Control
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          required
+          autoComplete="off"
+          value={password}
+          type="password"
+          placeholder="Password"
+        />
+      </FloatingLabel>
+      <Button onClick={verifyUser} variant="outline-success">
+        Login
+      </Button>
+      {/* <div className="login_box">
         <div className="group1">
           <form onSubmit={verifyUser} className="form">
             <input
@@ -63,7 +91,7 @@ const Login = () => {
             <button>Login</button>
           </form>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };

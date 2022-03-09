a47721c7740de1659fec6ab38be9ffa8b4f96bc1
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-03-09 22:25:09 +0300

    backend getAllVideosByChannelId

--- backend/controllers/channel.js
+++ backend/controllers/channel.js
@@ -21,7 +21,34 @@ const createNewChannel = (req, res) => {
     });
   });
 };
+// ================================== //
 
+// This function get all videos from videos
+const getAllVideoByChannelId = (req, res) => {
+  const id = req.params.id;
+  const query = `SELECT * FROM videos WHERE channel_id = ?`;
+  const data = [id];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    }
+    if (!result) {
+      return res.status(200).json({
+        success: false,
+        message: `No videos Yet`,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: `all the videos with channel id ${id}`,
+      result: result,
+    });
+  });
+};
 module.exports = {
+  getAllVideoByChannelId,
   createNewChannel,
 };
--- backend/routes/channel.js
+++ backend/routes/channel.js
@@ -3,11 +3,15 @@ const express = require("express");
 const channelRouter = express.Router();
 
 const { authentication } = require("../middleware/authentication");
-const { createNewChannel } = require("../controllers/channel");
+const {
+  createNewChannel,
+  getAllVideoByChannelId,
+} = require("../controllers/channel");
 
 //   ===========================
 
 channelRouter.post("/", authentication, createNewChannel);
+channelRouter.get("/:id", authentication, getAllVideoByChannelId);
 
 //   ===========================
 
